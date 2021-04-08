2502eecab1dc32db847fdda0c48363cdd48af3a0
Douglas Socorro <juancarlossocorro571@gmail.com>
2021-04-07 21:57:42 -0430

    I've been finished the "Musixmatch" scraper.

--- src/musixmatch-scraper.js
+++ src/musixmatch-scraper.js
@@ -1,10 +1,10 @@
 const { JSDOM } = require('jsdom')
 
-function extractLyric(html, knownInfo = {}) {
+exports.extractLyricData = function (html, knownInfo = {}) {
   const page = JSDOM.fragment(html)
 
   let url = ''
-  let track = ''
+  let track = 0
   let title = ''
   let artist = []
   let album = ''
@@ -14,10 +14,10 @@ function extractLyric(html, knownInfo = {}) {
     url = knownInfo.url
 
   } else {
-    url = page.querySelector('link[rel="amphtml"]').getAttribute('href')
+    url = page.querySelector('meta[property="al:web:url"]').getAttribute('content')
   }
 
-  if(knownInfo.track) {
+  if(knownInfo.track !== undefined) {
     track = knownInfo.track
 
   } else {
@@ -30,14 +30,18 @@ function extractLyric(html, knownInfo = {}) {
   } else {
     title = page.querySelector('h1.mxm-track-title__track').textContent
     title = title.replace('Letra', '')
+
+    if(title.indexOf(' (fe') > -1) title = title.slice(0, title.indexOf(' (fe'))
   }
 
   if(knownInfo.artist) { 
     artist = knownInfo.artist;
 
   } else {
-    let art = page.querySelector('a.mxm-track-title__artist').textContent
-    artist.push(art)
+    let art = page.querySelectorAll('a.mxm-track-title__artist-link')
+    for(let link of art) {
+      artist.push(link.textContent)
+    }
   }
 
   if(knownInfo.album) {
@@ -66,4 +70,42 @@ function extractLyric(html, knownInfo = {}) {
   }
 }
 
-exports.extractLyric = extractLyric;
+exports.extractAlbumData = function (html, knownInfo = {}) {
+  const albumData = {}
+  const page = JSDOM.fragment(html)
+
+  if(knownInfo.name) {
+    albumData.name = knownInfo.name
+
+  } else {
+    albumData.name = page.querySelector('div.mxm-album-banner__title h1').textContent
+  }
+
+  if(knownInfo.artist) {
+    albumData.artist = knownInfo.artist
+  } else {
+    albumData.artist = page.querySelector('h2.mxm-album-banner__artist').textContent
+  }
+
+  if(knownInfo.url) {
+    albumData.url = knownInfo.url
+  } else {
+    albumData.url = page.querySelector('meta[property="og:url"]').getAttribute('content')
+  }
+
+  albumData.lyricsToDownload = []
+
+  let tracks = page.querySelectorAll('li[id*="track_"]')
+  tracks.forEach( track => {
+    let lyric = {}
+
+    lyric.state = 1
+    lyric.track = Number(track.querySelector('div.mui-cell__index-view').textContent)
+    lyric.url = 'https://www.musixmatch.com' + track.querySelector('a').getAttribute('href')
+    lyric.title = track.querySelector('h2.mui-cell__title').textContent
+
+    albumData.lyricsToDownload.push(lyric)
+  })
+
+  return albumData
+}
--- src/musixmatch-scraper.test.js
+++ src/musixmatch-scraper.test.js
@@ -61,9 +61,8 @@ describe.only('Musixmatch scraper use case', async () => {
 
     expect(html).to.be.a('string')
 
-    const resivedData = mms.extractLyric(html, { 
-      track: 1 , 
-      url: 'https://www.musixmatch.com/es/letras/Justin-Bieber/Ghost'
+    const resivedData = mms.extractLyricData(html, { 
+      track: 1
     });
 
 
@@ -75,4 +74,245 @@ describe.only('Musixmatch scraper use case', async () => {
     expect(resivedData.lyric).to.be.equal(expectedData.lyric)
   })
   
+  it('Should extract multiple artist', async () => {
+    const expectedData = {
+      artist: ['Taylor Swift', 'Bon Iver'],
+
+      url: 'https://www.musixmatch.com/es/letras/Taylor-Swift-Bon-Iver/exile-Bon-Iver',
+      title: 'exile',
+      album: 'folklore',
+      lyric: stripIndents`I can see you standing, honey
+                          With his arms around your body
+                          Laughin', but the joke's not funny at all
+                          And it took you five whole minutes
+                          To pack us up and leave me with it
+                          Holdin' all this love out here in the hall
+
+                          I think I've seen this film before
+                          And I didn't like the ending
+                          You're not my homeland anymore
+                          So what am I defending now?
+                          You were my town
+                          Now I'm in exile, seein' you out
+                          I think I've seen this film before
+
+                          I can see you starin', honey
+                          Like he's just your understudy
+                          Like you'd get your knuckles bloody for me
+                          Second, third, and hundredth chances
+                          Balancin' on breaking branches
+                          Those eyes add insult to injury
+
+                          I think I've seen this film before
+                          And I didn't like the ending
+                          I'm not your problem anymore
+                          So who am I offending now?
+                          You were my crown
+                          Now I'm in exile, seein' you out
+                          I think I've seen this film before
+                          So I'm leavin' out the side door
+
+                          So step right out, there is no amount
+                          Of crying I can do for you
+                          All this time
+                          We always walked a very thin line
+                          You didn't even hear me out (You didn't even hear me out)
+                          You never gave a warning sign (I gave so many signs)
+
+                          All this time
+                          I never learned to read your mind (Never learned to read my mind)
+                          I couldn't turn things around (You never turned things around)
+                          'Cause you never gave a warning sign (I gave so many signs)
+                          So many signs, so many signs
+                          You didn't even see the signs
+
+                          I think I've seen this film before
+                          And I didn't like the ending
+                          You're not my homeland anymore
+                          So what am I defending now?
+                          You were my town
+                          Now I'm in exile, seein' you out
+                          I think I've seen this film before
+                          So I'm leavin' out the side door
+
+                          So step right out, there is no amount
+                          Of crying I can do for you
+                          All this time
+                          We always walked a very thin line
+                          You didn't even hear me out (Didn't even hear me out)
+                          You never gave a warning sign (I gave so many signs)
+
+                          All this time
+                          I never learned to read your mind (Never learned to read my mind)
+                          I couldn't turn things around (You never turned things around)
+                          'Cause you never gave a warning sign (I gave so many signs)
+
+                          All this time (So many signs)
+                          I never learned to read your mind (So many signs)
+                          I couldn't turn things around (I couldn't turn things around)
+                          'Cause you never gave a warning sign (You never gave a warning sign)
+                          You never gave a warning sign
+                          Ah, ah`
+
+    }
+
+    const html = util.readPage(path.join(__dirname, '../test-files', 'mms-l2.html'))
+    let resivedData = mms.extractLyricData(html)
+
+    //Most important test
+    expect(resivedData.artist).to.be.deep.equal(expectedData.artist)
+
+    //Checks for the other data...
+    expect(resivedData.url).to.be.equal(expectedData.url)
+    expect(resivedData.track).to.be.equal(0)
+    expect(resivedData.title).to.be.equal(expectedData.title)
+    expect(resivedData.album).to.be.equal(expectedData.album)
+    expect(resivedData.lyric).to.be.equal(expectedData.lyric)
+  })
+  
+  it('Should return the exact lyric passed to the "knownInfo" param', async () => {
+    const expectedData = {
+      url: 'https://www.musixmatch.com/es/letras/Taylor-Swift-Bon-Iver/exile-Bon-Iver',
+      track: 2,
+      title: 'exile',
+      artist: ['Taylor Swift', 'Bon Iver'],
+      album: 'folklore',
+      lyric: 'some lyric...'
+    }
+
+    const resivedData = mms.extractLyricData('', expectedData)
+
+    expect(resivedData).to.be.deep.equal(expectedData)
+  })
+
+  it('Should extract the album data from html', async () => {
+    const expectedData = {
+      name: 'folklore',
+      artist: 'Taylor Swift',
+      url: 'https://www.musixmatch.com/es/album/Taylor-Swift/folklore',
+      lyricsToDownload: [
+        { 
+          track: 1,
+          title: 'the 1',
+          url: 'https://www.musixmatch.com/es/letras/Taylor-Swift/the-1',
+          state: 1
+        },
+        { 
+          track: 2,
+          title: 'cardigan',
+          url: 'https://www.musixmatch.com/es/letras/Taylor-Swift/cardigan',
+          state: 1
+        },
+        { 
+          track: 3,
+          title: 'the last great american dynasty',
+          url: 'https://www.musixmatch.com/es/letras/Taylor-Swift/the-last-great-american-dynasty',
+          state: 1
+        },
+        { 
+          track: 4,
+          title: 'exile (feat. Bon Iver)',
+          url: 'https://www.musixmatch.com/es/letras/Taylor-Swift-Bon-Iver/exile-Bon-Iver',
+          state: 1
+        },
+        { 
+          track: 5,
+          title: 'my tears ricochet',
+          url: 'https://www.musixmatch.com/es/letras/Taylor-Swift/my-tears-ricochet',
+          state: 1
+        },
+        { 
+          track: 6,
+          title: 'mirrorball',
+          url: 'https://www.musixmatch.com/es/letras/Taylor-Swift/mirrorball',
+          state: 1
+        },
+        { 
+          track: 7,
+          title: 'seven',
+          url: 'https://www.musixmatch.com/es/letras/Taylor-Swift/seven',
+          state: 1
+        },
+        { 
+          track: 8,
+          title: 'august',
+          url: 'https://www.musixmatch.com/es/letras/Taylor-Swift/august',
+          state: 1
+        },
+        { 
+          track: 9,
+          title: 'this is me trying',
+          url: 'https://www.musixmatch.com/es/letras/Taylor-Swift/this-is-me-trying',
+          state: 1
+        },
+        { 
+          track: 10,
+          title: 'illicit affairs',
+          url: 'https://www.musixmatch.com/es/letras/Taylor-Swift/illicit-affairs',
+          state: 1
+        },
+        { 
+          track: 11,
+          title: 'invisible string',
+          url: 'https://www.musixmatch.com/es/letras/Taylor-Swift/invisible-string',
+          state: 1
+        },
+        { 
+          track: 12,
+          title: 'mad woman',
+          url: 'https://www.musixmatch.com/es/letras/Taylor-Swift/mad-woman',
+          state: 1
+        },
+        { 
+          track: 13,
+          title: 'epiphany',
+          url: 'https://www.musixmatch.com/es/letras/Taylor-Swift/epiphany',
+          state: 1
+        },
+        { 
+          track: 14,
+          title: 'betty',
+          url: 'https://www.musixmatch.com/es/letras/Taylor-Swift/betty',
+          state: 1
+        },
+        { 
+          track: 15,
+          title: 'betty',
+          url: 'https://www.musixmatch.com/es/letras/Taylor-Swift/betty-1',
+          state: 1
+        },
+        { 
+          track: 16,
+          title: 'peace',
+          url: 'https://www.musixmatch.com/es/letras/Taylor-Swift/peace',
+          state: 1
+        },
+        { 
+          track: 17,
+          title: 'hoax',
+          url: 'https://www.musixmatch.com/es/letras/Taylor-Swift/hoax',
+          state: 1
+        }
+      ]
+    }
+
+    const html = util.readPage(path.join(__dirname, '../test-files', 'mms-a2.html'))
+
+    const resivedData = mms.extractAlbumData(html)
+
+    expect(resivedData.name).to.be.equal(expectedData.name)
+    expect(resivedData.artist).to.be.equal(expectedData.artist)
+    expect(resivedData.url).to.be.equal(expectedData.url)
+
+    expect(resivedData.lyricsToDownload).to.be.deep.equal(expectedData.lyricsToDownload)
+  })
+
 })
+
+//TO-DOs
+
+/*
+
+  - add function to extract album list of lyrics
+  - add function to extract multiple artist of a lyric
+*/
